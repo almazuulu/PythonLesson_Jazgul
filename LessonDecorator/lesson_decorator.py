51d79963e559929b563e3043a72417f8cc5507f6
almazuulu <almazuulu@gmail.com>
2025-03-21 17:11:09 +0800

    decorators

--- LessonDecorator/lesson_decorator.py
+++ LessonDecorator/lesson_decorator.py
@@ -1,23 +1,28 @@
-def decorator(f):
-    def wrapper(*args, **kwargs):
-        res = f(*args)
-        
-        with open('hello.txt', "w", encoding="utf8") as f:
-            f.write(res)
-            f.write('\n')
+def file_decorator(filename):
+    def decorator(fun):
+        def wrapper(*args, **kwargs):
+            res = fun(*args, **kwargs)
             
-    return wrapper
+            with open(filename, "w", encoding="utf8") as f:
+                f.write(res)
+                f.write('\n')
+                
+            return res
+                
+        return wrapper
+    return decorator
     
-@decorator
+@file_decorator("hello.txt")
 def say_hello(n):
     result_txt = f"Hello {n}!"
     return result_txt
     
-@decorator
+@file_decorator("math.txt")
 def math_func(n1, n2):
     result_math = f"Результат сложения:{n1 + n2}"
     return result_math
-    
 
+    
 say_hello("Elena")
+math_func(15, 20)
 
