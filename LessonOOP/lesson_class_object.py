f956ff3838c3e2bb6b119ef4b5f2235b8944c83f
almazuulu <almazuulu@gmail.com>
2025-03-25 23:46:16 +0800

    HW for Lesson 1 OOP

--- LessonOOP/lesson_class_object.py
+++ LessonOOP/lesson_class_object.py
@@ -21,23 +21,36 @@ def display_car(brand, model, year):
 
 ########### OOP подход #################
 # Создание классов и объектов
+from datetime import datetime
 
 class Car:
     def __init__(self, brand, model, year):
         self.brandLocal = brand
         self.modelLocal = model
         self.yearLocal = year
+        self.current_year = datetime.now().year
         
     def display_info(self):
         print(f"{self.brandLocal} {self.modelLocal}, {self.yearLocal} года")
 
+    def calculate_year(self, year_after):
+        print(f"After {year_after} your car {self.modelLocal} will be {(self.current_year - self.yearLocal) + year_after}")
 
+    def locations_go(self, listTrip, numberPeople):
+        print(f"List of trip: {listTrip} in {self.modelLocal} with {numberPeople} people")
+    
 car1 = Car("BMW", "X5", 2019)
 car2 = Car("Toyota", "Camry", 2022)
+
 
 car1.display_info()
 car2.display_info()
 
+car1.calculate_year(10)
+car2.calculate_year(10)
+
+car1.locations_go(["Bishkek", "Osh", "Talas"], 5)
+car2.locations_go(["IK", "Naryn", "Batken"], 3)
 
 
 
