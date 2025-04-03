ac26eb009864b70881f8456b85b522e5ab0ea762
almazuulu <almazuulu@gmail.com>
2025-04-03 22:50:41 +0800

    AbstractExample

--- LessonOOP/abstractExample.py
+++ LessonOOP/abstractExample.py
@@ -12,3 +12,22 @@
 Каждый инструмент должен по-своему выводить сообщение о том, как он играет.
 """
 
+from abc import ABC, abstractmethod
+
+class MusicalInstrument(ABC):
+    
+    @abstractmethod
+    def make_sound(self):
+        pass 
+    
+
+class Guitar(MusicalInstrument):
+    def __init__(self, sound):
+        self.sound = sound
+        
+    def make_sound(self):
+        print(f"Guitar makes sound like: '{self.sound}'")
+        
+
+guitar = Guitar("zzzz-zzz-zz")
+guitar.make_sound()
